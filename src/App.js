170ae6bcf1e6137c190f3c9f3ec19f6244a54893
unknown <Rashed.al6armoom@gmail.com>
2022-05-12 01:13:22 +0300

    finished task 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
+import products from "./products.js"
+
+function Product(props){
+return(
+  <div class= "product-name">
+  <img class= "product-image" src={props.image}/>
+  <h5>{props.name}</h5>
+  <h5>{props.price}</h5>
+    </div>
+)
+}
 
 function App() {
+  const items = products.map((product) => (
+  <Product name={product.name} 
+  price={product.price} 
+  image={product.image} />));
+
   return (
     <div className="App">
-      <h1>what lyric</h1>
+      <h1>what lyric?</h1>
       <h4> bits and beats</h4>
       <img
         src="https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg"
         alt=""
       />
+      <div className="product-list">
+    {items}
+    </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+      name: "margherita pizza   ",
+      price: "4KD",
+      image:
+        "https://www.abeautifulplate.com/wp-content/uploads/2015/08/the-best-homemade-margherita-pizza-1-4.jpg",
+    },
+    {
+      name: "   pasta red sauce",
+      price: "5KD",
+      image:
+        "https://img.taste.com.au/9Rr43HzQ/w1200-h630-cfill/taste/2016/11/pasta-with-simple-tomato-sauce-21073-1.jpeg",
+    },,
+  ];
+
+  export default products;
